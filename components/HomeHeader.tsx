e186e8622b753e71480a30aa9c46271ee6525389
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2026-01-06 16:16:52 -0300

    Efeito de onda na hero

--- components/HomeHeader.tsx
+++ components/HomeHeader.tsx
@@ -1,11 +1,20 @@
 import styles from '@/styles/HomeHeader.module.css';
+import WavePattern from "@/components/WavePattern";
 
 function homeHeader() {
   return (
-    <header className={styles.container}>
-      <h1 className={styles['main-title']}>Gustavo <br /> Mazurkiewitz</h1>
-      <h2 className={styles.subtitle}>Desenvolvedor</h2>
-    </header>
+      <div
+        style={{
+          overflowX: 'hidden'
+        }}
+      >
+        <WavePattern isFlipped/>
+        <header className={styles.container}>
+          <h1 className={styles['main-title']}>Gustavo <br /> Mazurkiewitz</h1>
+          <h2 className={styles.subtitle}>Desenvolvedor</h2>
+        </header>
+        <WavePattern />
+      </div> 
   )
 }
 
