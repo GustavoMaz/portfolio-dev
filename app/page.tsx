a71f478df03b1ff4e97ae1ccdab3c5adc1e8214b
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2026-01-09 15:14:34 -0300

    Iniciando o desenvolvimento da seção Sobre Mim e importando algumas imagens

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import HomeHeader from "@/components/HomeHeader";
-import ProjectsGallery from "@/components/ProjectGallery";
+import ProjectGallery from "@/components/ProjectGallery";
+import About from "@/components/About";
 
 export default function Home() {
   return <>
     <HomeHeader />
-    <ProjectsGallery />
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam consequuntur nesciunt, impedit consequatur delectus totam. Aut dolores natus velit, eius veniam atque rem at unde, facilis, ad perspiciatis officiis aliquam?</p>
+    <ProjectGallery />
+    <About />
   </>;
 }
--- components/About.tsx
+++ components/About.tsx
@@ -1,7 +1,14 @@
 function About() {
   return (
     <section id='about'>
-      <h2></h2>
+      <h2>Um pouco sobre mim</h2>
+      <div>
+        <p>Olá, eu sou o Gustavo! Atualmente moro em Três Coroas, RS e busco por uma oportunidade de trabalhar na área de desenvolvimento e programação.</p>
+        <p>Desde criança eu sempre gostei muito de computadores, jogos e números. A partir daí eu me interessei primeiramente por desenvolvimento de jogos, o que me levou a ter meus primeiros contatos com programação e arte digital. Com os meus 13 anos de idade, eu sabia o suficiente para criar <span lang="en">sprites</span> e mecânicas básicas de jogos.</p>
+        <img src="gladiator.gif" alt="Sprite de gladiador andando" />
+        <img src="slash.gif" alt="Sprite de samurai atacando com a espada" />
+        <img src="tentacles.png" alt="Cavaleiro em uma masmorra" />
+      </div>
     </section>
   )
 }
