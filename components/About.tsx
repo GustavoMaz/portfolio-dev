404fa5ba6c803d5bf28efb4f65f3f374285bb151
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2026-01-11 10:03:08 -0300

    Adicionando o texto da seção Sobre Mim

--- components/About.tsx
+++ components/About.tsx
@@ -1,16 +1,70 @@
 function About() {
   return (
-    <section id='about'>
+    <section id="about">
       <h2>Um pouco sobre mim</h2>
       <div>
-        <p>Olá, eu sou o Gustavo! Atualmente moro em Três Coroas, RS e busco por uma oportunidade de trabalhar na área de desenvolvimento e programação.</p>
-        <p>Desde criança eu sempre gostei muito de computadores, jogos e números. A partir daí eu me interessei primeiramente por desenvolvimento de jogos, o que me levou a ter meus primeiros contatos com programação e arte digital. Com os meus 13 anos de idade, eu sabia o suficiente para criar <span lang="en">sprites</span> e mecânicas básicas de jogos.</p>
+        <p>
+          Olá, eu sou o Gustavo! Atualmente moro em Três Coroas, RS e busco por
+          uma oportunidade de trabalhar na área de desenvolvimento e
+          programação.
+        </p>
+        <p>
+          Desde criança eu sempre gostei muito de computadores, jogos e números.
+          A partir daí eu me interessei primeiramente por desenvolvimento de
+          jogos, o que me levou a ter meus primeiros contatos com programação e
+          arte digital. Com os meus 13 anos de idade, eu sabia o suficiente para
+          criar <span lang="en">sprites</span> e mecânicas básicas de jogos.
+        </p>
         <img src="gladiator.gif" alt="Sprite de gladiador andando" />
         <img src="slash.gif" alt="Sprite de samurai atacando com a espada" />
         <img src="tentacles.png" alt="Cavaleiro em uma masmorra" />
       </div>
+      <div>
+        <p>
+          Minha matéria preferida na escola sempre foi a matemática, o que ao
+          longo dos me levou a tornar-me{" "}
+          <a href="#" title="Ver certificados">
+            <strong>
+              bimedalhista nacional das Olímpiadas Brasileiras de Matemática
+              &#40;OBMEP&#41;
+            </strong>
+          </a>{" "}
+          e{" "}
+          <a href="#" title="Ver certificado">
+            <strong>
+              medalhista de ouro a nível nacional das Olimpíadas Brasileiras de
+              Matemática Financeira &#40;OBMF&#41;
+            </strong>
+          </a>
+          , além da{" "}
+          <a href="#" title="Ver certificado">
+            <strong>conclusão do PIC Júnior</strong>
+          </a>
+          , programa que desenvolve as habilidades de resolução de problemas,
+          lógica e algoritmos, onde obtive uma média final de 8,8/10 nas
+          avaliações.
+        </p>
+      </div>
+      <div>
+        <p>
+          Devido ao meu longo tempo de exposição à programação, já tive contato
+          com diversas linguagens, frameworks e ferramentas, então não tenho muita
+          dificuldade em me adaptar a diferentes ambientes de desenvolvimento.
+          Entretanto, as ferramentas principais que escolho para usar nos
+          meus projetos são:
+          <ul>
+            <li>React</li>
+            <li>Vue.js</li>
+            <li>TypeScript</li>
+            <li>Next.js</li>
+            <li>Framer Motion</li>
+            <li>Git</li>
+          </ul>
+          Também já tive contato com ferramentas de back-end, como PHP e MySQL.
+        </p>
+      </div>
     </section>
-  )
+  );
 }
 
-export default About
+export default About;
