2f79e510ef4feb1e6e6bb26bbb8fa92cdfb16362
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2026-01-13 19:50:10 -0300

    Removendo a fonte padrão do nextjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import MainNavigation from "@/components/MainNavigation";
 import MainFooter from "@/components/MainFooter";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Gustavo Mazurkiewitz - Desenvolvedor",
@@ -28,10 +17,9 @@ export default function RootLayout({
     <html lang="pt-br">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Reddit+Sans+Condensed:wght@200..900&display=swap" rel="stylesheet" />
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <MainNavigation />
         {children}
         <MainFooter />
